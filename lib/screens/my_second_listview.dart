3df69ac622faecd36624e18fffa74a316d74c683
mhrhabib <mhrhabib39@gmail.com>
2022-01-06 18:42:26 +0600

    Update my_second_listview.dart

--- lib/screens/my_second_listview.dart
+++ lib/screens/my_second_listview.dart
@@ -1,19 +1,15 @@
 import 'package:flutter/material.dart';
 
-class MySecondListView extends StatelessWidget {
+class MySecondListView extends StatefulWidget {
+  @override
+  State<MySecondListView> createState() => _MySecondListViewState();
+}
+
+class _MySecondListViewState extends State<MySecondListView> {
+  bool isChecked = false;
+
   List numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  List<String> imageUrl = [
-    'https://picsum.photos/200/300',
-    'https://picsum.photos/200/300',
-    'https://picsum.photos/200/300',
-    'https://picsum.photos/200/300',
-    'https://picsum.photos/200/300',
-    'https://picsum.photos/200/300',
-    'https://picsum.photos/200/300',
-    'https://picsum.photos/200/300',
-    'https://picsum.photos/200/300',
-    'https://picsum.photos/200/300',
-  ];
+
   List<Color> colors = [
     Colors.red,
     Colors.green,
@@ -32,24 +28,40 @@ class MySecondListView extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: SizedBox(
-          height: MediaQuery.of(context).size.height * .17,
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
+          //height: MediaQuery.of(context).size.height * .17,
+          child: ListView.separated(
+            //scrollDirection: Axis.horizontal,
             itemCount: numbers.length,
             itemBuilder: (BuildContext context, int n) {
-              return CircleAvatar(
-                backgroundColor: colors[n],
-                backgroundImage: NetworkImage(imageUrl[n]),
-                radius: 50,
-                child: Text(
-                  '${numbers[n]}',
-                  style: const TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
+              return ListTile(
+                leading: CircleAvatar(
+                  backgroundColor: colors[n],
+                  //backgroundImage: NetworkImage(imageUrl[n]),
+                  radius: 50,
+                  child: Text(
+                    '${numbers[n]}',
+                    style: const TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
+                ),
+                title: const Text('This is the title'),
+                subtitle: const Text('Subtitle subtitle'),
+                trailing: Checkbox(
+                  onChanged: (value) {
+                    setState(() {
+                      isChecked = value!;
+                    });
+                  },
+                  value: isChecked,
                 ),
               );
             },
+            separatorBuilder: (context, index) => const Divider(
+              thickness: 1,
+              color: Colors.grey,
+            ),
           ),
         ),
       ),
